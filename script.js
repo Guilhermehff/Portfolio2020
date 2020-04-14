3f2f45d2c99618208a440c77f7a6110475e3feea
Guilherme Fidélio <guilhermeh1996@gmail.com>
2020-04-14 19:52:48 -0300

    Overlay implemented

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
   $(".isuna").click(function(e) {
 
+    $(".overlay").css("pointer-events", "all");
     $(".project-isuna").css("pointer-events", "all");
+    $(".overlay").animate({
+      "opacity": "0.24"
+    }, 500);
     $(".project-isuna").animate({
       "margin-top": "0%",
       "opacity": "1"
@@ -13,15 +17,35 @@ $(document).ready(function() {
   });
 
 
+  $(".overlay").click(function(e) {
 
+    $(".project").css("pointer-events", "none");
+    $(".overlay").css("pointer-events", "none");
+    $(".project").animate({
+      "margin-top": "60%",
+      "opacity": "0"
+    }, 500);
+    $(".overlay").animate({
+      "opacity": "0"
+    }, 500);
+
+
+    e.preventDefault();
+
+
+  });
 
 
 
   $(".exit-project").click(function(e) {
 
     $(this).parent().css("pointer-events", "none");
+    $(".overlay").css("pointer-events", "none");
     $(this).parent().animate({
       "margin-top": "60%",
+      "opacity": "0"
+    }, 500);
+    $(".overlay").animate({
       "opacity": "0"
     }, 500);
 
