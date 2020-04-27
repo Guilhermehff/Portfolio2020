445bb42451cbf9a68418792f24786d669921c2ae
Guilherme Fidélio <guilhermeh1996@gmail.com>
2020-04-26 21:33:42 -0300

    Optimized hover effect on home

--- script.js
+++ script.js
@@ -3,240 +3,224 @@ $(document).ready(function() {
   var lastId;
   var project;
   var menuItems = $(".summary").find("a");
-
-  // Anchors corresponding to menu items
-  scrollItemsIsuna = $(".project-isuna .summary").find("a").map(function() {
-    var item = $($(this).attr("href"));
-    if (item.length) {
-      return item;
-    }
-  });
+  var isunaColor = '#FF708A';
+  var btwaColor = '#FEE27F';
+  var hubsterColor = '#FE844C';
+  var accompColor = '#8DEEA2';
 
 
-  $(".isuna").hover(
+  $(".selected-projects-info div").hover(
     function() {
-      $('.rotate-1').css('stroke', '#FF708A');
-      $('.rotate-2').css('stroke', '#FF708A');
-    },
-    function() {
-      $('.rotate-1').css('stroke', '#A7BFD3');
-      $('.rotate-2').css('stroke', '#A7BFD3');
-    }
-  );
 
-  $(".btwa").hover(
-    function() {
-      $('.rotate-1').css('stroke', '#FEE27F');
-      $('.rotate-2').css('stroke', '#FEE27F');
-    },
-    function() {
-      $('.rotate-1').css('stroke', '#A7BFD3');
-      $('.rotate-2').css('stroke', '#A7BFD3');
-    }
-  );
-
-  $(".hubster").hover(
-    function() {
-      $('.rotate-1').css('stroke', '#FE844C');
-      $('.rotate-2').css('stroke', '#FE844C');
-    },
-    function() {
-      $('.rotate-1').css('stroke', '#A7BFD3');
-      $('.rotate-2').css('stroke', '#A7BFD3');
-    }
-  );
-
-  $(".paper-arg").hover(
-    function() {
-      $('.rotate-1').css('stroke', '#8DEEA2');
-      $('.rotate-2').css('stroke', '#8DEEA2');
-    },
-    function() {
-      $('.rotate-1').css('stroke', '#A7BFD3');
-      $('.rotate-2').css('stroke', '#A7BFD3');
-    }
-  );
-
-
-  $(".selected-projects-info div").click(function(e) {
-
-    project = ".project-" + $(this).attr('class');
-
-    if ($(this).attr('class') === "isuna") {
-      $(".panel-1, .panel-2, .panel-3, .panel-4").css("background-color", "#FF708A");
-    } else if ($(this).attr('class') === "hubster") {
-      $(".panel-1, .panel-2, .panel-3, .panel-4").css("background-color", "#FE844C");
-    }
-
-
-    $(project).css("display", "block");
-    $(project).css("pointer-events", "all");
-    $(".container-vertical").css("pointer-events", "none");
-    $(".scroll").css("overflow", "overlay");
-    $(".scroll").scrollTop(0);
-    $(".panel-1").animate({
-      "left": "0%"
-    }, 500);
-    $(".panel-2").delay(100).animate({
-      "left": "0%"
-    }, 500);
-    $(".panel-3").delay(200).animate({
-      "left": "0%"
-    }, 500);
-    $(".panel-4").delay(300).animate({
-      "left": "0%"
-    }, 500);
-
-
-    $(project).delay(700).animate({
-      "opacity": "1"
-    }, 0);
-    $(".container-vertical").delay(700).animate({
-      "opacity": "0"
-    }, 0);
-    $("body").delay(700).animate({
-      "background-color": "white"
-    }, 0);
-
-
-    $(".panel-1").delay(200).animate({
-      "right": "100%"
-    }, 500);
-    $(".panel-2").delay(300).animate({
-      "right": "100%"
-    }, 500);
-    $(".panel-3").delay(400).animate({
-      "right": "100%"
-    }, 500);
-    $(".panel-4").delay(500).animate({
-      "right": "100%"
-    }, 500);
-
-    e.preventDefault();
-
-
-  });
-
-
-
-
-  $(".exit").click(function(e) {
-
-    var project = "." + $(this).parent().attr('class').split(' ').pop();;
-
-    $(project).css("pointer-events", "none");
-    $(".container-vertical").css("pointer-events", "all");
-    $(".scroll").css("overflow", "overlay");
-
-    $(".panel-1").animate({
-      "right": "0%"
-    }, 500);
-    $(".panel-2").delay(100).animate({
-      "right": "0%"
-    }, 500);
-    $(".panel-3").delay(200).animate({
-      "right": "0%"
-    }, 500);
-    $(".panel-4").delay(300).animate({
-      "right": "0%"
-    }, 500);
-
-
-    $(project).delay(700).animate({
-      "opacity": "0"
-    }, 0);
-    $(".container-vertical").delay(700).animate({
-      "opacity": "1"
-    }, 0);
-    $("body").delay(700).animate({
-      "background-color": "#F4FAFF"
-    }, 0);
-
-
-    $(".panel-1").delay(200).animate({
-      "left": "100%"
-    }, 500);
-    $(".panel-2").delay(300).animate({
-      "left": "100%"
-    }, 500);
-    $(".panel-3").delay(400).animate({
-      "left": "100%"
-    }, 500);
-    $(".panel-4").delay(500).animate({
-      "left": "100%"
-    }, 500);
-
-
-    setTimeout(function() {
-      $(project).css("display", "none");
-    }, 1000);
-
-
-    e.preventDefault();
-
-
-  });
-
-
-
-
-
-  // Bind click handler to menu items
-  // so we can get a fancy scroll animation
-  menuItems.click(function(e) {
-
-    var href = $(this).attr("href"),
-      offsetTop = href === "#" ? 0 : $(href).position().top;
-
-    $('.scroll').stop().animate({
-      scrollTop: offsetTop + 10
-    }, 850);
-
-    e.preventDefault();
-
-  });
-
-
-  // Bind to scroll
-  $(".scroll").scroll(function() {
-    // Get container scroll position
-    var fromTop = $(this).scrollTop();
-
-
-    if (project == ".project-isuna") {
-
-      // Get id of current scroll item
-      var cur = scrollItemsIsuna.map(function() {
-        if ($(this).position().top - $(window).height() / 8 < fromTop)
-          return this;
-      });
-      // Get the id of the current element
-      cur = cur[cur.length - 1];
-      var id = cur && cur.length ? cur[0].id : "";
-      var lastId;
-
-      if (lastId !== id) {
-        lastId = id;
-        // Set/remove active class
-        menuItems
-          .parent().removeClass("active")
-          .end().filter("[href='#" + id + "']").parent().addClass("active");
+      if ($(this).attr('class') == "isuna") {
+        $('.rotate-1').css('stroke', isunaColor);
+        $('.rotate-2').css('stroke', isunaColor);
+      } else if ($(this).attr('class') == "hubster") {
+        $('.rotate-1').css('stroke', hubsterColor);
+        $('.rotate-2').css('stroke', hubsterColor);
+      } else if ($(this).attr('class') == "btwa") {
+        $('.rotate-1').css('stroke', btwaColor);
+        $('.rotate-2').css('stroke', btwaColor);
+      } else if ($(this).attr('class') == "btwa") {
+        $('.rotate-1').css('stroke', btwaColor);
+        $('.rotate-2').css('stroke', btwaColor);
       }
+    },
 
-      if ($(".scroll").scrollTop() + $(".scroll").innerHeight() >= $(".project")[0].scrollHeight) {
-        menuItems
-          .parent().removeClass("active")
-        $(".summary li:last-child").addClass("active");
-        lastId = "end";
-      }
+    function() {
+      $('.rotate-1').css('stroke', '#A7BFD3');
+      $('.rotate-2').css('stroke', '#A7BFD3');
+    });
 
+
+
+
+
+
+$(".selected-projects-info div").click(function(e) {
+
+  project = ".project-" + $(this).attr('class');
+
+  if ($(this).attr('class') === "isuna") {
+    $(".panel-1, .panel-2, .panel-3, .panel-4").css("background-color", "#FF708A");
+  } else if ($(this).attr('class') === "hubster") {
+    $(".panel-1, .panel-2, .panel-3, .panel-4").css("background-color", "#FE844C");
+  }
+
+
+  $(project).css("display", "block");
+  $(project).css("pointer-events", "all");
+  $(".container-vertical").css("pointer-events", "none");
+  $(".scroll").css("overflow", "overlay");
+  $(".scroll").scrollTop(0);
+  $(".panel-1").animate({
+    "left": "0%"
+  }, 500);
+  $(".panel-2").delay(100).animate({
+    "left": "0%"
+  }, 500);
+  $(".panel-3").delay(200).animate({
+    "left": "0%"
+  }, 500);
+  $(".panel-4").delay(300).animate({
+    "left": "0%"
+  }, 500);
+
+
+  $(project).delay(700).animate({
+    "opacity": "1"
+  }, 0);
+  $(".container-vertical").delay(700).animate({
+    "opacity": "0"
+  }, 0);
+  $("body").delay(700).animate({
+    "background-color": "white"
+  }, 0);
+
+
+  $(".panel-1").delay(200).animate({
+    "right": "100%"
+  }, 500);
+  $(".panel-2").delay(300).animate({
+    "right": "100%"
+  }, 500);
+  $(".panel-3").delay(400).animate({
+    "right": "100%"
+  }, 500);
+  $(".panel-4").delay(500).animate({
+    "right": "100%"
+  }, 500);
+
+  e.preventDefault();
+
+
+});
+
+// Anchors corresponding to menu items
+scrollItemsIsuna = $(".project-isuna .summary").find("a").map(function() {
+  var item = $($(this).attr("href"));
+  if (item.length) {
+    return item;
+  }
+});
+
+$(".exit").click(function(e) {
+
+  var project = "." + $(this).parent().attr('class').split(' ').pop();;
+
+  $(project).css("pointer-events", "none");
+  $(".container-vertical").css("pointer-events", "all");
+  $(".scroll").css("overflow", "overlay");
+
+  $(".panel-1").animate({
+    "right": "0%"
+  }, 500);
+  $(".panel-2").delay(100).animate({
+    "right": "0%"
+  }, 500);
+  $(".panel-3").delay(200).animate({
+    "right": "0%"
+  }, 500);
+  $(".panel-4").delay(300).animate({
+    "right": "0%"
+  }, 500);
+
+
+  $(project).delay(700).animate({
+    "opacity": "0"
+  }, 0);
+  $(".container-vertical").delay(700).animate({
+    "opacity": "1"
+  }, 0);
+  $("body").delay(700).animate({
+    "background-color": "#F4FAFF"
+  }, 0);
+
+
+  $(".panel-1").delay(200).animate({
+    "left": "100%"
+  }, 500);
+  $(".panel-2").delay(300).animate({
+    "left": "100%"
+  }, 500);
+  $(".panel-3").delay(400).animate({
+    "left": "100%"
+  }, 500);
+  $(".panel-4").delay(500).animate({
+    "left": "100%"
+  }, 500);
+
+
+  setTimeout(function() {
+    $(project).css("display", "none");
+  }, 1000);
+
+
+  e.preventDefault();
+
+
+});
+
+
+
+
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e) {
+
+  var href = $(this).attr("href"),
+    offsetTop = href === "#" ? 0 : $(href).position().top;
+
+  $('.scroll').stop().animate({
+    scrollTop: offsetTop + 10
+  }, 850);
+
+  e.preventDefault();
+
+});
+
+
+// Bind to scroll
+$(".scroll").scroll(function() {
+  // Get container scroll position
+  var fromTop = $(this).scrollTop();
+
+
+  if (project == ".project-isuna") {
+
+    // Get id of current scroll item
+    var cur = scrollItemsIsuna.map(function() {
+      if ($(this).position().top - $(window).height() / 8 < fromTop)
+        return this;
+    });
+    // Get the id of the current element
+    cur = cur[cur.length - 1];
+    var id = cur && cur.length ? cur[0].id : "";
+    var lastId;
+
+    if (lastId !== id) {
+      lastId = id;
+      // Set/remove active class
+      menuItems
+        .parent().removeClass("active")
+        .end().filter("[href='#" + id + "']").parent().addClass("active");
     }
 
+    if ($(".scroll").scrollTop() + $(".scroll").innerHeight() >= $(".project")[0].scrollHeight) {
+      menuItems
+        .parent().removeClass("active")
+      $(".summary li:last-child").addClass("active");
+      lastId = "end";
+    }
+
+  }
 
 
 
 
-  });
+
+});
 
 
 });
